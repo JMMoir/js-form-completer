aec7b92de09584e077d22a5dd1e645a1b6da9eda
John Moir <John.Moir@nes.scot.nhs.uk>
2023-11-02 17:16:37 +0000

    remove unused code

--- formCompleter.js
+++ formCompleter.js
@@ -15,16 +15,3 @@ javascript: (function() {
         dd[i].selectedIndex = 1;
     }
 })();
-
-
-javascript: (function() {
-    var el = document.getElementsByTagName('input');
-    for (var i =0; i < el.length; i++){
-        if (el[i].type == 'checkbox' || el[i].type == 'radio')
-            el[i].checked = true;
-    }
-    var dd = document.getElementsByTagName('select');
-    for (var i = 0; i < dd.length; i++){
-        dd[i].selectedIndex = 1;
-    }
-})();
